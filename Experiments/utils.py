d2ea2fbe56c6371874640191e390675bda4f953b
“Juan <juanfgallo@experimentality.com>
2021-01-12 18:31:21 -0500

    adding utils files and validation metrics

---
+++ Experiments/utils.py
@@ -0,0 +1,9 @@
+class Utils ():
+    def __init__(self):
+            pass
+    
+    def try_to_configure(self):
+        return alguna_cosa_pa_retornar()
+
+def alguna_cosa_pa_retornar():
+    return "eres un encanto"
